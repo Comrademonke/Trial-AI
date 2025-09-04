b32d13f6e6021ecf6ffb297e8d71ec800a0f0b2b
Sophie Clark <scla255@aucklanduni.ac.nz>
2025-09-04 12:27:49 +1200

    Added a new image and moved around some of the items. Also implemented speech bubble mechanism.

--- src/main/java/nz/ac/auckland/se206/controllers/AiWitnessChatController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/AiWitnessChatController.java
@@ -4,7 +4,10 @@ import java.io.File;
 import javafx.animation.PauseTransition;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
@@ -18,7 +21,21 @@ import nz.ac.auckland.apiproxy.exceptions.ApiProxyException;
 public class AiWitnessChatController extends ChatControllerCentre {
 
   @FXML private TextArea txtaChat;
+  @FXML private Slider slider;
   @FXML private VBox flashbackMessage;
+  @FXML private ImageView speechBubble1;
+  @FXML private ImageView speechBubble2;
+  @FXML private ImageView speechBubble3;
+  @FXML private ImageView speechBubble4;
+  @FXML private ImageView speechBubble5;
+  @FXML private ImageView speechBubble6;
+  @FXML private ImageView speechBubble7;
+  @FXML private ImageView speechBubble8;
+  @FXML private ImageView speechBubble9;
+  @FXML private ImageView speechBubble10;
+  @FXML private ImageView speechBubble11;
+  @FXML private ImageView speechBubble12;
+  @FXML private Button clearNoiseBtn;
 
   @Override
   @FXML
@@ -29,6 +46,16 @@ public class AiWitnessChatController extends ChatControllerCentre {
       e.printStackTrace();
     }
     flashbackMessage.setVisible(true);
+    hideAllSpeechBubbles();
+    clearNoiseBtn.setVisible(false);
+
+    slider
+        .valueProperty()
+        .addListener(
+            (obs, oldVal, newVal) -> {
+              showSpeechBubble(newVal.intValue());
+            });
+
     Platform.runLater(
         () -> {
           PauseTransition pause = new PauseTransition(Duration.seconds(1));
@@ -41,5 +68,58 @@ public class AiWitnessChatController extends ChatControllerCentre {
           mediaPlayer.play();
           pause.play();
         });
+  }
+
+  private void hideAllSpeechBubbles() {
+    speechBubble1.setVisible(false);
+    speechBubble2.setVisible(false);
+    speechBubble3.setVisible(false);
+    speechBubble4.setVisible(false);
+    speechBubble5.setVisible(false);
+    speechBubble6.setVisible(false);
+    speechBubble7.setVisible(false);
+    speechBubble8.setVisible(false);
+    speechBubble9.setVisible(false);
+    speechBubble10.setVisible(false);
+    speechBubble11.setVisible(false);
+    speechBubble12.setVisible(false);
+  }
+
+  private void showSpeechBubble(int value) {
+    hideAllSpeechBubbles();
+    if (value >= 2) {
+      speechBubble2.setVisible(true);
+    }
+    if (value >= 3) {
+      speechBubble1.setVisible(true);
+    }
+    if (value >= 4) {
+      speechBubble7.setVisible(true);
+    }
+    if (value >= 5) {
+      speechBubble4.setVisible(true);
+    }
+    if (value >= 6) {
+      speechBubble8.setVisible(true);
+    }
+    if (value >= 7) {
+      speechBubble5.setVisible(true);
+    }
+    if (value >= 8) {
+      speechBubble3.setVisible(true);
+    }
+    if (value >= 9) {
+      speechBubble9.setVisible(true);
+      speechBubble10.setVisible(true);
+    }
+    if (value >= 10) {
+      speechBubble6.setVisible(true);
+      speechBubble11.setVisible(true);
+    }
+    if (value >= 11) {
+      speechBubble12.setVisible(true);
+      clearNoiseBtn.setVisible(true);
+      slider.setVisible(false);
+    }
   }
 }
