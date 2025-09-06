15ed38a0826e6ab36442afe495f63993ff15553e
Andrew Chen <ache337@aucklanduni.ac.nz>
2025-09-06 21:34:11 +1200

    feat: overhauled previous structure and created new fxml file for the memory slideshow

---
+++ src/main/java/nz/ac/auckland/se206/controllers/DefendantMemoryController.java
@@ -0,0 +1,59 @@
+package nz.ac.auckland.se206.controllers;
+
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import nz.ac.auckland.apiproxy.exceptions.ApiProxyException;
+import nz.ac.auckland.se206.App;
+import nz.ac.auckland.se206.TimerManager;
+
+public class DefendantMemoryController {
+
+  @FXML private Label timer;
+  @FXML private ProgressBar progressBar;
+
+  @FXML
+  public void initialize() throws ApiProxyException {
+    TimerManager timer = TimerManager.getInstance();
+
+    // Set initial state immediately
+    this.timer.setText(TimerManager.formatTime(timer.getSecondsRemainingProperty().get()));
+    progressBar.progressProperty().bind(timer.getProgressProperty());
+    applyColor(timer.getProgressProperty().get());
+
+    // Bind updates
+    timer
+        .getSecondsRemainingProperty()
+        .addListener(
+            (obs, oldVal, newVal) -> {
+              this.timer.setText(TimerManager.formatTime(newVal.intValue()));
+            });
+
+    timer
+        .getProgressProperty()
+        .addListener(
+            (obs, oldVal, newVal) -> {
+              applyColor(newVal.doubleValue());
+            });
+  }
+
+  private void applyColor(double progress) {
+    // Progress bar changes colour depending on the time
+    String color = TimerManager.getAccentColor(progress);
+    progressBar.setStyle("-fx-accent: " + color + ";");
+  }
+
+  /**
+   * Navigates back to the previous view.
+   *
+   * @param event the action event triggered by the go back button
+   * @throws ApiProxyException if there is an error communicating with the API proxy
+   * @throws IOException if there is an I/O error
+   */
+  @FXML
+  private void onGoBack(ActionEvent event) throws ApiProxyException, IOException {
+    App.setRoot("room");
+  }
+}
