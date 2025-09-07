2110de7420c5bab220b2a261a79a3728f1786b10
Andrew Chen <ache337@aucklanduni.ac.nz>
2025-09-07 17:08:21 +1200

    feat: finished rough memory scene for defendant

--- src/main/java/nz/ac/auckland/se206/controllers/DefendantMemoryController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/DefendantMemoryController.java
@@ -1,13 +1,18 @@
 package nz.ac.auckland.se206.controllers;
 
 import java.io.IOException;
+import javafx.animation.PauseTransition;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.Slider;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 import nz.ac.auckland.apiproxy.exceptions.ApiProxyException;
 import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.TimerManager;
@@ -22,9 +27,15 @@ public class DefendantMemoryController {
   @FXML private StackPane scene4;
   @FXML private StackPane scene5;
   @FXML private StackPane scene6;
+  @FXML private StackPane scene7;
+  @FXML private StackPane scene8;
+  @FXML private StackPane scene9;
+  @FXML private StackPane scene11;
   @FXML private Slider slidingBar;
   @FXML private ImageView image1;
   @FXML private ImageView image2;
+  @FXML private Button proceedButton;
+  @FXML private VBox memoryMessage;
 
   @FXML
   public void initialize() throws ApiProxyException {
@@ -56,41 +67,83 @@ public class DefendantMemoryController {
             (obs, oldVal, newVal) -> {
               showDialogue(newVal.intValue());
             });
+    memoryMessage.setVisible(true);
+    Platform.runLater(
+        () -> {
+          PauseTransition pause = new PauseTransition(Duration.seconds(1));
+          pause.setOnFinished(e -> memoryMessage.setVisible(false));
+          pause.play();
+        });
   }
 
   private void showDialogue(int value) {
-    if (value == 0) {
-      scene1.setVisible(false);
-      scene2.setVisible(false);
-      scene3.setVisible(false);
-    }
-    if (value == 1) {
-      image1.setVisible(true);
-      scene1.setVisible(true);
-    } else if (value == 2) {
-      image1.setVisible(true);
-      scene2.setVisible(true);
-    } else if (value == 3) {
-      image1.setVisible(true);
-      scene3.setVisible(true);
-    } else if (value == 4) {
-      scene1.setVisible(false);
-      scene2.setVisible(false);
-      scene3.setVisible(false);
-      image1.setVisible(false);
-      scene4.setVisible(false);
-    } else if (value == 5) {
-      scene4.setVisible(true);
-      scene5.setVisible(false);
-    } else if (value == 6) {
-      scene4.setVisible(false);
-      scene5.setVisible(true);
-      scene6.setVisible(false);
-    } else if (value == 7) {
-      scene5.setVisible(false);
-      scene6.setVisible(true);
-    } else if (value == 8) {
-      scene6.setVisible(false);
+    scene1.setVisible(false);
+    scene2.setVisible(false);
+    scene3.setVisible(false);
+    scene4.setVisible(false);
+    scene5.setVisible(false);
+    scene6.setVisible(false);
+    scene7.setVisible(false);
+    scene8.setVisible(false);
+    scene9.setVisible(false);
+    scene11.setVisible(false);
+    image1.setVisible(false);
+    proceedButton.setVisible(false);
+
+    switch (value) {
+      case 0:
+        image1.setVisible(true);
+        break;
+      case 1:
+        image1.setVisible(true);
+        scene1.setVisible(true);
+        break;
+      case 2:
+        image1.setVisible(true);
+        scene1.setVisible(true);
+        scene2.setVisible(true);
+        break;
+      case 3:
+        image1.setVisible(true);
+        scene1.setVisible(true);
+        scene2.setVisible(true);
+        scene3.setVisible(true);
+        break;
+      case 5:
+        scene4.setVisible(true);
+        break;
+      case 6:
+        scene5.setVisible(true);
+        break;
+      case 7:
+        scene6.setVisible(true);
+        break;
+      case 8:
+        image1.setVisible(true);
+        break;
+      case 9:
+        image1.setVisible(true);
+        scene7.setVisible(true);
+        break;
+      case 10:
+        image1.setVisible(true);
+        scene7.setVisible(true);
+        scene8.setVisible(true);
+        break;
+      case 12:
+        scene4.setVisible(true);
+        break;
+      case 13:
+        scene9.setVisible(true);
+        break;
+      case 14:
+        scene11.setVisible(true);
+        break;
+      case 15:
+        proceedButton.setVisible(true);
+        break;
+      default:
+        break;
     }
   }
 
@@ -108,7 +161,7 @@ public class DefendantMemoryController {
    * @throws IOException if there is an I/O error
    */
   @FXML
-  private void onGoBack(ActionEvent event) throws ApiProxyException, IOException {
-    App.setRoot("room");
+  private void onProceed(ActionEvent event) throws ApiProxyException, IOException {
+    App.setRoot("defendant");
   }
 }
